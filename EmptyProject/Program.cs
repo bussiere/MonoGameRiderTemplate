23bf464600fc1518d3040404b74fe548f8a99bf9
bussiere <bussiere@gmail.com>
2017-04-19 00:23:45 +0200

    beginning

---
+++ EmptyProject/Program.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Example1
+{
+if WINDOWS || LINUX
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+endif
+}
