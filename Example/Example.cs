727fa832f1e854950cc1fef6bee953d3dd6c5884
bussiere <bussiere@gmail.com>
2017-04-19 01:31:40 +0200

    trying to add dlls

---
+++ Example/Example.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Example
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
